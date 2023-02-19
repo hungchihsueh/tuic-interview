10cbad8e74aa65c8dc59c22d9090ef86f7357ca7
hungchihsueh <84211143+hungchihsueh@users.noreply.github.com>
2023-02-19 16:35:07 +0800

    Update Login.vue

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -7,25 +7,10 @@
 	const registerWithGoogle = () => {
 		signInWithPopup(auth, provider)
 			.then((result) => {
-				console.log(result);
-				// This gives you a Google Access Token. You can use it to access the Google API.
-				const credential = GoogleAuthProvider.credentialFromResult(result);
-				const token = credential.accessToken;
-				// The signed-in user info.
-				// IdP data available using getAdditionalUserInfo(result)
-				// ...
 				router.push("/todo");
 			})
 			.catch((error) => {
 				console.log(error);
-				// Handle Errors here.
-				const errorCode = error.code;
-				const errorMessage = error.message;
-				// The email of the user's account used.
-				const email = error.customData.email;
-				// The AuthCredential type that was used.
-				const credential = GoogleAuthProvider.credentialFromError(error);
-				// ...
 			});
 	};
 </script>
