329a52b4f50ea7051954eb159d407ec332f907e8
hungchihsueh <84211143+hungchihsueh@users.noreply.github.com>
2023-02-19 10:53:55 +0800

    udpate

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,27 @@
 <script setup>
 	// router
-	import { useRouter } from "vue-router";
+	import { useRouter, useRoute } from "vue-router";
 
 	const router = useRouter();
+	const route = useRoute();
 	const goTodo = () => {
 		router.push("/todo");
 	};
+	console.log(route.path);
 </script>
 
 <template>
+	<router-link
+		to=""
+		@click="
+			() => {
+				route.path == `/map` ? router.back() : router.push(`/map`);
+			}
+		"
+		class="fixed top-5 right-5 p-3 px-5 rounded shadow bg-green-400 text-white font-bold"
+		>{{ route.path == "/map" ? "Back" : "Map Demo" }}</router-link
+	>
+
 	<router-view></router-view>
 </template>
 
--- src/views/Todos.vue
+++ src/views/Todos.vue
@@ -40,7 +40,6 @@
 		}
 	};
 	const toggoleTodo = (e, todo) => {
-		console.log(todo.todoKey);
 		const db = getDatabase();
 		const user = auth.currentUser;
 		console.log(user);
@@ -107,37 +106,56 @@
 			);
 		});
 	});
+	const checki = (i) => {
+		console.log(i);
+	};
 </script>
 <template>
 	<div class="container transition-all duration-150 mx-auto pt-10">
-		<button @click="signOutByebye">signout</button>
-		<div class="flex justify-center items-center gap-3 mx-auto">
+		<button
+			@click="signOutByebye"
+			class="py-3 px-5 rounded shadow bg-gray-50 font-bold hover:bg-blue-500 hover:text-white transition-all duration-150">
+			signout
+		</button>
+		<div
+			class="mx-auto text-center my-10 font-extrabold text-transparent text-5xl bg-clip-text bg-gradient-to-br from-green-400 to-blue-600">
+			Hello! {{ auth.currentUser.displayName }}
+		</div>
+		<div
+			class="mb-10 w-1/2 flex justify-center items-center gap-3 mx-auto border shadow-md p-3 rounded-full bg-gray-100">
 			<input
-				class="border border-black p-3 rounded-md"
+				class="border-none shadow-[inset_0_4px_6px_-1px_rgba(0,0,0,0.1),0_1px_2px_-1px_rgba(0,0,0,0.1)] p-3 rounded-l-full w-4/5"
 				v-model="input"
 				type="text" />
 			<button
 				@click="addTodo"
-				class="shadow border rounded-sm p-3">
+				class="shadow border rounded-r-full p-3 bg-orange-300 text-white font-bold w-1/5 min-w-fit hover:shadow-md transition-all duration-150 active:shadow-transparent">
 				Add
 			</button>
 		</div>
-		<div class="bg-blue-400">
-			<ul>
+		<div class="w-1/2 px-2 mx-auto">
+			<ul class="flex flex-col justify-start items-start gap-3">
 				<li
 					v-for="(todo, i) in state.todos"
-					:key="`${todo}${i}`">
-					<div>
+					:key="`${todo}${i}`"
+					class="flex w-full justify-between items-center rounded-full shadow font-bold text-xl transition-colors"
+					:class="[todo.isFinished ? `bg-green-300` : `bg-blue-300`]">
+					<div class="flex gap-5 p-5 justify-start items-center">
 						<input
 							:checked="todo.isFinished"
 							:aria-label="todo.todo"
 							type="checkbox"
 							name=""
 							id=""
+							class="w-8 h-8 rounded-full"
 							@change="(e) => toggoleTodo(e, todo)" />
-						{{ todo.todo }}
-						<button @click="() => clearToDo(todo)">x</button>
+						<span class="text-2xl">{{ todo.todo }}</span>
 					</div>
+					<button
+						@click="() => clearToDo(todo)"
+						class="px-5 hover:text-red-500 hover:text-3xl transition-all duration-150">
+						x
+					</button>
 				</li>
 			</ul>
 		</div>
